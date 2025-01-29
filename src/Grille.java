2b17586c9bd3ef0b94cc5a54d46a08556c17bd72
imenmiladi <94322807+imenmiladi@users.noreply.github.com>
2025-01-29 19:38:03 +0100

    changer la structure grille composée par des noeuds dés le debut

--- src/Grille.java
+++ src/Grille.java
@@ -1,39 +1,35 @@
 import java.util.*;
 
 public class Grille {
-    private char[][] grille;
+    private Noeud[][] grille;
     private int ligne;
     private int colonne;
 
-
+    // Constructeur
     Grille(int ligne, int colonne) {
         this.ligne = ligne;
         this.colonne = colonne;
-        grille = new char[ligne][colonne];
+        grille = new Noeud[ligne][colonne];
     }
-
 
     public void creerGrille(List<String> dic) {
         Random rand = new Random();
 
-        // Ajouter des murs et initialiser les cases dans la grille
+        // Initialiser la grille avec des nœuds et des murs
         for (int i = 0; i < ligne; i++) {
             for (int j = 0; j < colonne; j++) {
-                if (rand.nextDouble() < 0.2) { // 20% murs
-                    grille[i][j] = '#';
-                } else {
-                    grille[i][j] = '\0'; // Case vide
-                }
+                char lettre = rand.nextDouble() < 0.2 ? '#' : '\0'; // 20% de murs
+                Noeud.TypeNoeud typeNoeud = (lettre == '#') ? Noeud.TypeNoeud.bloquante : Noeud.TypeNoeud.passante;
+                grille[i][j] = new Noeud(i, j, lettre, typeNoeud);
             }
         }
 
-        //prendre quelques mot aleatoirement du dic
+        // Prendre quelques mots aléatoirement du dictionnaire
         Collections.shuffle(dic);
         List<String> mots = new ArrayList<>();
         for (int i = 0; i < Math.min(Math.max(ligne, colonne), dic.size()); i++) {
             mots.add(dic.get(i));
         }
-
 
         // Placer les mots dans les cases passantes
         for (String mot : mots) {
@@ -54,11 +50,14 @@ public class Grille {
         // Remplir les cases vides restantes avec des lettres aléatoires
         for (int i = 0; i < ligne; i++) {
             for (int j = 0; j < colonne; j++) {
-                if (grille[i][j] == '\0') {
-                    grille[i][j] = (char) ('A' + rand.nextInt(26));
+                if (grille[i][j].getLettre() == '\0') {
+                    grille[i][j].setLettre((char) ('A' + rand.nextInt(26)));
                 }
             }
         }
+
+        // Créer les graphes des nœuds (ajouter les voisins)
+        //creerGraphGrille();
     }
 
     // Essayer de placer un mot à la position (x, y) dans la direction spécifiée
@@ -68,35 +67,35 @@ public class Grille {
         if (direction == 0) { // horizontale
             if (y + len > colonne) return false;
             for (int i = 0; i < len; i++) {
-                if (grille[x][y + i] != '\0' && grille[x][y + i] != mot.charAt(i)) {
+                if (grille[x][y + i].getLettre() != '\0' && grille[x][y + i].getLettre() != mot.charAt(i)) {
                     return false;
                 }
             }
             // Placer le mot
             for (int i = 0; i < len; i++) {
-                grille[x][y + i] = mot.charAt(i);
+                grille[x][y + i].setLettre(mot.charAt(i));
             }
         } else if (direction == 1) { // verticale
             if (x + len > ligne) return false;
             for (int i = 0; i < len; i++) {
-                if (grille[x + i][y] != '\0' && grille[x + i][y] != mot.charAt(i)) {
+                if (grille[x + i][y].getLettre() != '\0' && grille[x + i][y].getLettre() != mot.charAt(i)) {
                     return false;
                 }
             }
             // Placer le mot
             for (int i = 0; i < len; i++) {
-                grille[x + i][y] = mot.charAt(i);
+                grille[x + i][y].setLettre(mot.charAt(i));
             }
         } else if (direction == 2) { // diagonale
             if (x + len > ligne || y + len > colonne) return false;
             for (int i = 0; i < len; i++) {
-                if (grille[x + i][y + i] != '\0' && grille[x + i][y + i] != mot.charAt(i)) {
+                if (grille[x + i][y + i].getLettre() != '\0' && grille[x + i][y + i].getLettre() != mot.charAt(i)) {
                     return false;
                 }
             }
             // Placer le mot
             for (int i = 0; i < len; i++) {
-                grille[x + i][y + i] = mot.charAt(i);
+                grille[x + i][y + i].setLettre(mot.charAt(i));
             }
         }
         return true;
@@ -104,12 +103,11 @@ public class Grille {
 
 
 
-
+    // Afficher la grille en termes de lettres
     public void afficherGrille() {
-
         for (int i = 0; i < ligne; i++) {
             for (int j = 0; j < colonne; j++) {
-                System.out.print(grille[i][j] + " ");
+                System.out.print(grille[i][j].getLettre() + " ");
             }
             System.out.println();
         }
