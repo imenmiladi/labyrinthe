658fe3d0cc005b59a12c71ac2ea8162abcb4203a
imenmiladi <94322807+imenmiladi@users.noreply.github.com>
2025-01-29 18:29:13 +0100

    create grid from dic

--- src/Grille.java
+++ src/Grille.java
@@ -1,34 +1,120 @@
 import java.util.*;
 
 public class Grille {
-   private char[][] grille;
-   private int ligne;
-   private int colonne;
+    private char[][] grille;
+    private int ligne;
+    private int colonne;
 
-   Grille(int ligne, int colonne) {
-       this.ligne = ligne;
-       this.colonne = colonne;
-       grille = new char[ligne][colonne];
-   }
 
-   public void creerGrille(Set<String> dic) {
+    Grille(int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
+        grille = new char[ligne][colonne];
+    }
 
-   for (int i = 0; i < ligne; i++) {
-       for (int j = 0; j < colonne; j++) {
-           grille[i][j] = '0';
-   }
-  }
-   }
 
-   public void afficherGrille() {
-       for (int i = 0; i < ligne; i++) {
-           for (int j = 0; j < colonne; j++) {
-               System.out.print(grille[i][j] + " ");
+    public void creerGrille(List<String> dic) {
+        Random rand = new Random();
 
-           }
-           System.out.println();
-       }
-   }
+        // Ajouter des murs et initialiser les cases dans la grille
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (rand.nextDouble() < 0.2) { // 20% murs
+                    grille[i][j] = '#';
+                } else {
+                    grille[i][j] = '\0'; // Case vide
+                }
+            }
+        }
+
+        //prendre quelques mot aleatoirement du dic
+        Collections.shuffle(dic);
+        List<String> mots = new ArrayList<>();
+        for (int i = 0; i < Math.min(Math.max(ligne, colonne), dic.size()); i++) {
+            mots.add(dic.get(i));
+        }
+
+
+        // Placer les mots dans les cases passantes
+        for (String mot : mots) {
+            boolean place = false;
+            int tentatives = 0;
+            while (!place && tentatives < 100) { // Limiter les tentatives à 100
+                int x = rand.nextInt(ligne);
+                int y = rand.nextInt(colonne);
+                int direction = rand.nextInt(3); // 0: horizontal, 1: vertical, 2: diagonal
+                place = PeutEtrePlacer(mot, x, y, direction);
+                tentatives++;
+            }
+            if (!place) {
+                System.out.println("Impossible de placer le mot: " + mot);
+            }
+        }
+
+        // Remplir les cases vides restantes avec des lettres aléatoires
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (grille[i][j] == '\0') {
+                    grille[i][j] = (char) ('A' + rand.nextInt(26));
+                }
+            }
+        }
+    }
+
+    // Essayer de placer un mot à la position (x, y) dans la direction spécifiée
+    private boolean PeutEtrePlacer(String mot, int x, int y, int direction) {
+        int len = mot.length();
+        // Vérifier si le mot peut être placé dans la grille sans sortir des limites
+        if (direction == 0) { // horizontale
+            if (y + len > colonne) return false;
+            for (int i = 0; i < len; i++) {
+                if (grille[x][y + i] != '\0' && grille[x][y + i] != mot.charAt(i)) {
+                    return false;
+                }
+            }
+            // Placer le mot
+            for (int i = 0; i < len; i++) {
+                grille[x][y + i] = mot.charAt(i);
+            }
+        } else if (direction == 1) { // verticale
+            if (x + len > ligne) return false;
+            for (int i = 0; i < len; i++) {
+                if (grille[x + i][y] != '\0' && grille[x + i][y] != mot.charAt(i)) {
+                    return false;
+                }
+            }
+            // Placer le mot
+            for (int i = 0; i < len; i++) {
+                grille[x + i][y] = mot.charAt(i);
+            }
+        } else if (direction == 2) { // diagonale
+            if (x + len > ligne || y + len > colonne) return false;
+            for (int i = 0; i < len; i++) {
+                if (grille[x + i][y + i] != '\0' && grille[x + i][y + i] != mot.charAt(i)) {
+                    return false;
+                }
+            }
+            // Placer le mot
+            for (int i = 0; i < len; i++) {
+                grille[x + i][y + i] = mot.charAt(i);
+            }
+        }
+        return true;
+    }
+
+
+
+
+    public void afficherGrille() {
+
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                System.out.print(grille[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class Main {
-    public static <Noued> void main(String[] args) {
+    public static <Noued> void main(String[] args) throws IOException {
         System.out.println("Hello world!");
          Noeud n = new Noeud(1,2,'f', Noeud.TypeNoeud.passante);
          System.out.println(n.toString());
          Grille g=new Grille(10,14);
-        Set<String> dic = new HashSet<>(Arrays.asList(
-                "APPLE", "BANANA", "ORANGE", "GRAPE", "PEACH",
-                "CHERRY", "MANGO", "LEMON", "LIME", "PLUM",
-                "KIWI", "PINEAPPLE", "COCONUT", "STRAWBERRY",
-                "BLUEBERRY", "RASPBERRY", "WATERMELON", "PAPAYA",
-                "APRICOT", "FIG"
-        ));
+
+        List<String> theme= Arrays.asList("food", "tech");
+        List<String> dic = ChargerDict.chargerDictionnaire("themes/"+theme.get(1)+".txt");
+        System.out.println("Dictionnaire chargé avec le theme " + theme.get(1)+ " avec "+((List<?>) dic).size() + " mots.");
+        System.out.println(dic);
 
         g.creerGrille(dic);
         g.afficherGrille();
