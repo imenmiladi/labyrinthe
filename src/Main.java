bfeeeb01f7a6d5e523d8a5abbefc7dec6926bbeb
imenmiladi <94322807+imenmiladi@users.noreply.github.com>
2025-02-07 20:24:48 +0100

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
+import javax.swing.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-         Noeud n = new Noeud(1,2,'f', Noeud.TypeNoeud.passante);
-         System.out.println(n.toString());
-         Grille g=new Grille(10,14);
 
-        List<String> theme= Arrays.asList("food", "tech");
-        List<String> dic = ChargerDict.chargerDictionnaire("themes/"+theme.get(1)+".txt");
-        System.out.println("Dictionnaire chargé avec le theme " + theme.get(1)+ " avec "+((List<?>) dic).size() + " mots.");
-        System.out.println(dic);
+        SwingUtilities.invokeLater(Menu::new);
 
-        g.creerGrille(dic);
-        g.afficherGrille();
-        g.afficherGraphe();
+
 
 
 
